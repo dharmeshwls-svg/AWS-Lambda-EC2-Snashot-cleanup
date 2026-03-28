fac2584dda1c557f002982cf4eff1e869368a2a5
anusha bambhrolia <anushabambhrolia@anushas-MacBook-Pro.local>
2026-03-28 13:14:49 -0500

    Code change

--- providers.tf
+++ providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+  # Configure the backend to store the Terraform state in an S3 bucket. Uncomment and provide the necessary values to enable remote state storage.
+  # backend "s3" {
+  #   bucket = var.s3_bucket
+  #   key    = "terraform.tfstate"
+  #   region = var.aws_region
+  # }
 }
 provider "aws" {
   region = var.aws_region
